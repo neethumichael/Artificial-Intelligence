b9f795f93ef75a3b93d661809d10e061d73bf20d
Neethu <neethu89@ccs.neu.edu>
2016-07-20 22:59:42 -0400

    updated handin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,25 +14,25 @@ import main.java.examples.poPacMan.POPacMan;
 public class Main {
 	public static POCommGhosts ghosts = null;
 	public static RandomGhosts randomGhosts = null;
-	
-    public static void main(String[] args) {
-    	
-    	// initializing BFS and AStar implementation objects
-    	BFSPacMan bfs = new BFSPacMan();
-    	AStarPacMan astar = new AStarPacMan(); 
-    	
-    	// initializing POGhosts and randomGhosts
-    	ghosts = new POCommGhosts(50);
-        randomGhosts =  new RandomGhosts();
 
-    	// intialization for Executor class.
-        // Update 1st argument as true for PO Environment
-        // Update 1st argument as false for fully observable environment
-        Executor executor = new Executor(false, true);
-         
-        // BFS ( uncomment the below call to runGameTimed to execute BFS search )
-       // executor.runGameTimed(bfs, ghosts, true);
-        // Astar ( uncomment the below call to runGameTimed to execute Astar search )
-        executor.runGameTimed(astar, ghosts, true);
-    }
+	public static void main(String[] args) {
+
+		// initializing BFS and AStar implementation objects
+		BFSPacMan bfs = new BFSPacMan();
+		AStarPacMan astar = new AStarPacMan(); 
+
+		// initializing POGhosts and randomGhosts
+		ghosts = new POCommGhosts(50);
+		randomGhosts =  new RandomGhosts();
+
+		// intialization for Executor class.
+		// Update 1st argument as true for PO Environment
+		// Update 1st argument as false for fully observable environment
+		Executor executor = new Executor(false, true);
+
+		// BFS ( uncomment the below call to runGameTimed to execute BFS search )
+		executor.runGameTimed(bfs, ghosts, true);
+		// Astar ( uncomment the below call to runGameTimed to execute Astar search )
+		//executor.runGameTimed(astar, ghosts, true);
+	}
 }
