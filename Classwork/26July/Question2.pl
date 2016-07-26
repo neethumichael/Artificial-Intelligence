03647e08bcada85477547022e19274aa8478e4ac
Neethu <neethu89@ccs.neu.edu>
2016-07-26 17:36:25 -0400

    classwork 26 July

---
+++ Classwork/26July/Question2.pl
@@ -0,0 +1,20 @@
+wife('Philp','Queen Elizabeth 11').
+son('Philip','Charles').
+son('Queen Elizabeth 11','Charles').
+wife('Charles,'Diana').
+wife('Charles,'Camilla').
+daugther('Philip','Anne'),
+daughter('Queen Elizabeth 11','Anne').
+son('Philip','Andrew').
+wife('Andrew','Sarah').
+son('Queen Elizabeth 11','Andrew').
+son('Philip',Edward').
+son('Queen Elizabeth 11','Edward').
+wife('Edward','Sophie').
+son('Diana','William').
+wife('William','Kate').
+son('Charles','
+
+
+
+
