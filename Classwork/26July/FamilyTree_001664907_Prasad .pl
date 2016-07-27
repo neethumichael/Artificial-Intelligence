8e5d8e61ca3b693972fa2b163fdaa753cb314e0b
Neethu <neethu89@ccs.neu.edu>
2016-07-27 00:00:50 -0400

    ques 2 updates.

--- Classwork/26July/FamilyTree_001664907_Prasad .pl
+++ Classwork/26July/FamilyTree_001664907_Prasad .pl
@@ -23,7 +23,6 @@ son('Harry','Diana').
 
 son('Peter Phillips','Captain Mark Phillips').
 son('Peter Phillips','Anne').
-son('Edward','James').
 son('George','William').
 son('George','Kate').
 son('James','Edward').
@@ -34,13 +33,13 @@ daughter('Anne','Queen Elizabeth 11').
 daughter('Zara Phillips','Anne').
 daughter('Zara Phillips','Captain Mark Phillips').
 
-daughter('Beatrice','Andrew').
-daughter('Andrew','Eugenie').
-daughter('Edward','Lousie').
-daughter('Peter','Savannah').
-daughter('Peter','Isla').
-daughter('Mike','Mia Grace').
-daughter('Anne','Zara').
+daughter('Savannah','Peter Phillips').
+daughter('Isla','Peter Phillips').
+daughter('Savannah','Autumn Phillips').
+daughter('Isla','Autumn Phillips').
+daughter('Mia Grace','Mike Tindall').
+daughter('Mia Grace','Zara Phillips').
+daughter('Zara Phillips','Anne').
 daughter('Beatrice','Andrew').
 daughter('Beatrice','Sarah').
 daughter('Eugenie','Andrew').
@@ -48,13 +47,6 @@ daughter('Eugenie','Sarah').
 daughter('Lousie','Edward').
 daughter('Lousie','Sophie').
 
-daughter('Peter Phillips','Savannah').
-daughter('Autumn Phillips','Savannah').
-daughter('Peter Phillips','Isla').
-daughter('Autumn Phillips','Isla').
-
-daughter('Mike Tindall','Mia Grace').
-daughter('Zara Phillips','Mia Grace').
 
 child(X,Y):- son(X,Y);daughter(X,Y).
 spouse(X,Y):- child(Z,X),child(Z,Y).
@@ -62,15 +54,17 @@ husband(X,Y):- wife(Y,X).
 parent(X,Y):- son(Y,X);daughter(Y,X).
 grandParent(X,Y):-parent(X,Z),parent(Z,Y).
 grandChild(X,Y):- child(X,Z),child(Z,Y).
-greatGrandChild(X,Y):- child(X,Z),grandChild(Z,Y).
-greatGrandParent(X,Y):- parent(X,Z),grandParent(Z,Y).
+greatGrandChild(X,Y):- child(X,W),grandChild(W,Y).
+greatGrandParent(X,Y):- parent(X,W),parent(W,Z),parent(Z,Y).
 brother(X,Y):-son(X,Z),son(Y,Z);daughter(Y,Z),son(X,Z).
 sister(X,Y):-daughter(X,Z),daughter(Y,Z);son(Y,Z),daughter(X,Z).
 halfSister(X,Y):- parent(Z,Y),spouse(Q,Z),not(parent(Q,Y)),daughter(X,Q).
 aunt(X,Y):- parent(Z,Y),sister(X,Z); parent(Z,Y),sisterInLaw(X,Z);halfSister(X,Y).
 halfBrother(X,Y):- parent(Z,Y),spouse(Q,Z),not(parent(Q,Y)),son(X,Q).
 uncle(X,Y):-parent(Z,Y),brother(X,Z); parent(Z,Y),brotherInLaw(X,Z);halfBrother(X,Y).
-brotherInLaw(X,Y):- brother(X,Z),spouse(Z,Y);sister(N,Y),spouse(X,N).
-sisterInLaw(X,Y):-sister(X,Z),spouse(Z,Y);brother(N,Y),spouse(X,N).
+brotherInLaw(X,Y):- brother(X,Z),spouse(Z,Y).
+sisterInLaw(X,Y):-sister(X,Z),spouse(Z,Y).
+niece(X,Y):- daughter(X,Z) ,brother(Z,Y); daughter(X,Z),sister(Z,Y);
+daughter(X,Z) ,brotherInLaw(Z,Y); daughter(X,Z),sisterInLaw(Z,Y).
 
 
